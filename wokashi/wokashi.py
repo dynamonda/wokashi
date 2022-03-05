821f51bae1542b1f775816fa0a4ed4e744773bf9
dynamonda <v7gj9kk@gmail.com>
2022-03-06 07:43:52 +0900

    [root] wokashi.get()にキャッシュのオンオフフラグを実装

--- wokashi/wokashi.py
+++ wokashi/wokashi.py
@@ -1,13 +1,12 @@
 import requests
+import requests_cache
 from bs4 import BeautifulSoup
 from wokashi.soup import MisoSoup
 
 
-def fuga() -> None:
-    print('fuga')
-
-
-def get(url: str) -> MisoSoup:
+def get(url: str, is_use_cache: bool = False) -> MisoSoup:
+    if is_use_cache == True:
+        requests_cache.install_cache('wokashi_cache')
     response: requests.Request = requests.get(url)
     soup = BeautifulSoup(response.content, features='lxml')
     return MisoSoup(soup, url)
